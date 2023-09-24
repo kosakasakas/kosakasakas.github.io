dd3d407680f250c40b4a1e6f812a3dfcda5e7b93
U-DENA\takahiro.kosaka <takahiro.kosaka@O-09474-WIN.dena.local>
2023-09-25 02:43:09 +0900

    Site updated: 2023-09-25 02:43:04

--- pokelist/ga_bridge.js
+++ pokelist/ga_bridge.js
@@ -1,24 +1,4 @@
 
-function readCsv(data) {
-  var target = '#csv-body';
-  var csv = $.csv.toArrays(data);
-  var insert = '';
-  $(csv).each(function() {
-    if (this.length > 0) {
-      insert += '<tr>';
-      $(this).each(function() {
-        insert += '<td>' + this + '</td>';
-      });
-      insert += '</tr>';
-    }
-  });
-  $(target).append(insert);
-}
-var csvfile = 'test.csv';
-$(function(){
-  $.get(csvfile, readCsv, 'text');
-});
-
 function readCSVData(filePath, successCallback, failCallback)
 {
   $.get( filePath, function(data) {
