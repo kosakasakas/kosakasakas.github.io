f12cd6e9ce365fe667b3ae63a0007483a209908f
U-DENA\takahiro.kosaka <takahiro.kosaka@O-09474-WIN.dena.local>
2024-02-03 04:33:11 +0900

    Site updated: 2024-02-03 04:33:04

---
+++ db/poke/trans_text.js
@@ -0,0 +1,13 @@
+"use strict";		
+var trans_text = {		
+"s Moves (select one to show detailed results)"	: 	" のわざ",
+"guaranteed OHKO"	: 	"確定1発KO",
+"% chance to OHKO"	: 	"%で1発KO",
+"% chance to "	: 	"%で",
+"HKO"	: 	"発KO",
+"guaranteed "	: 	"確定",
+" turns"	: 	"ターンで",
+"KO in "	: 	"KO ",
+"possible KO in "	: 	"KOかも ",
+"not a KO"	: 	"KO出来ず",
+}		
